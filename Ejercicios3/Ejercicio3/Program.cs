2b387e75831d9fd984349d57d4261c782ade9248
Agustín Galarza <45016200+AgustinArielGalarza@users.noreply.github.com>
2022-08-30 21:03:06 -0300

    Add files via upload
    Ejercicio 3

---
+++ Ejercicios3/Ejercicio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+public class Program
+{
+    public static void Main()
+    {
+        DimeColor("blanco");
+        
+
+        }
+    public static string DimeColor(string color)
+    {
+        string rta = "";
+        switch (color)
+        {
+            case "blanco":
+                return rta = "El color es blanco";
+                break;
+            case "azul":
+                return rta = "El color es azul";
+                break;
+            case "negro":
+                return rta = "El color es negro";
+                break;
+        }
+        return rta = "Vuelva a seleccionar";
+    }
+
+}
