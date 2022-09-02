95244f374dbd64c7786cf8873654b8ac7534e803
Agustin Galarza <agustin.galarza@zentricx.com>
2022-09-02 19:02:50 -0300

    Tp1-Terminado

---
+++ GeneralidadesTest/MainGeneralidadesTest.cs
@@ -0,0 +1,23 @@
+using Generalidades;
+namespace GeneralidadesTest
+{
+    [TestClass]
+    public class MainGeneralidadesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Punto1 punto1 = new Punto1();
+            Punto2 punto2 = new Punto2();
+            Punto3 punto3 = new Punto3();
+            Punto4 punto4 = new Punto4();
+            Punto5 punto5 = new Punto5();
+
+            Assert.IsNotNull(punto1);
+            Assert.IsNotNull(punto2);
+            Assert.IsNotNull(punto3);
+            Assert.IsNotNull(punto4);
+            Assert.IsNotNull(punto5);
+        }
+    }
+}
---
+++ ProgramacionAvanzadaII/Punto6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversiones
+{
+    public class Punto6
+    {
+        public void rtaPunto6()
+        {
+            Console.WriteLine("- Punto 6 -");
+            Console.WriteLine("6) ¿Cuál es la diferencia entre la sentencia for y foreach? ¿Cuándo se debe utilizar cada una de ellas? \n");
+            Console.WriteLine("La diferencia principal entre el las sentencias for y el foreach es que, el for recorre un array hasta la posicion que indicamos que recorrra," +
+                " en cambio el foreach recorre de principio a fin. \r\n\r\nAdemas, foreach no es un bucle si no mas bien un constructor el cual solo puede iterar en arrays y objetos." +
+                " For es bucle que nosotros podemos decir hasta donde se tiene que repetir. \r\n\r\nForeach no se puede utilizar para recorrer strings. \n");
+        }
+    }
+}
